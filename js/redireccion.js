6aa902cf259c77d04e258ae1e547530e622ef403
facundocameto <facucameto2014@gmail.com>
2020-08-20 21:59:58 -0300

    index user

--- js/redireccion.js
+++ js/redireccion.js
@@ -14,4 +14,26 @@ function redireccion(){
 
 exist();
 redireccion();
-console.log(user);
+document.addEventListener("DOMContentLoaded", function(e){
+  var usuario = document.getElementById("usuario");
+  usuario.innerHTML = user;
+  collapseExample.style.display = "none";
+});
+var asd = "";
+var collapseExample = document.getElementById("collapseExample");
+
+function rotate()
+{
+  var arrow_img = document.getElementById("arrow_img");
+  if (asd === "" || asd === "1")
+  {
+    asd = "0";
+    arrow_img.style.transform = "rotate(180deg)";
+    collapseExample.style.display = "block"
+  }
+  else {
+    asd = "1";
+    arrow_img.style.transform = "rotate(0deg)";
+    collapseExample.style.display = "none";
+  }
+}
