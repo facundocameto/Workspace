5854d261f4f424aaeff45322118d680f043f7316
facundocameto <68877564+facundocameto@users.noreply.github.com>
2020-09-30 23:32:28 -0300

    cart update

--- js/cart.js
+++ js/cart.js
@@ -7,23 +7,30 @@ function showCartList(cartJson)
         let cartProduct = cartArray[i];
 
             htmlContentToAppend += `
-                <div class="row list-group-item-action mb-5">
+                <div class="row list-group-item-action mb-5 p-2">
                     <div class="col-sm-2">
-                        <img width="80px" height="80px" src="` + cartProduct.src + `" class="img-thumbnail">
+                        <img width="170px" height="170px" src="` + cartProduct.src + `" class="img-thumbnail">
                     </div>
-                    <div class="col-9">
+                    <div class="col-8">
                         <div class="row">
-                            <h4 class="col-sm mb-1">`+ cartProduct.name +`</h4>
-                            <p class="col-sm">` + cartProduct.currency + " " + cartProduct.unitCost + `</p>
-                            <div class="pl-3 pr-3"><input style="max-width: 71px;" onclick="currentCost(` + i + `, ` + cartProduct.unitCost + `)" id="itemQuant` + i + `" class="col-2 form-control" type="number" value="` + cartProduct.count + `"></div>
+                            <h4 class="col-sm-4">`+ cartProduct.name +`</h4>
+                            <p class="col-sm-4">` + cartProduct.currency + " " + cartProduct.unitCost + `</p>
+                            <div class="col-sm-4">` +
+                            //quantity input
+                            `
+                            <input style="max-width: 71px;"  onkeyup="currentCost(` + i + `, ` + cartProduct.unitCost + `, ` + "`" + cartProduct.currency + "`" + `)"
+                            onclick="currentCost(` + i + `, ` + cartProduct.unitCost + `, ` + "`" + cartProduct.currency + "`" + `)" id="itemQuant` + i + `" class="col-2 form-control" type="number" value="` + cartProduct.count + `">` +
+                            //end of quantity input
+                            `
+                            </div>
                         </div>
                     </div>
-                    <p id="costPlaceholder` + i + `" class="col-1 p-0"></p>
+                    <div style="flex-grow: 1; max-width: 100%;" class="col-2 p-0 text-right"><p id="costPlaceholder` + i + `"></p></div>
                 </div>
             `;
             getJSONData(CART_INFO_URL).then(function(resultObj){
                 if (resultObj.status === "ok"){
-                    currentCost(i, cartProduct.unitCost);
+                    currentCost(i, cartProduct.unitCost, cartProduct.currency);
                 }
             });
     }
@@ -31,11 +38,38 @@ function showCartList(cartJson)
     document.getElementById("cart-list-container").innerHTML = htmlContentToAppend;
 }
 
-function currentCost(num, unitCost)
+function currentCost(num, unitCost, currency)
 {
+  if (currency == "UYU")
+  {
   var itemQuant = document.getElementById("itemQuant" + num).value;
   var costPlaceholder = document.getElementById("costPlaceholder" + num);
-  costPlaceholder.innerHTML = itemQuant*unitCost;
+  var subtotal = itemQuant*unitCost;
+  var subtotalUYU = "UYU " + subtotal;
+  costPlaceholder.innerHTML = subtotalUYU;
+
+  newCost[num] = subtotal;
+
+  }
+  else
+  {
+    var itemQuant = document.getElementById("itemQuant" + num).value;
+    var costPlaceholder = document.getElementById("costPlaceholder" + num);
+    var subtotal = itemQuant*unitCost*40;
+    var subtotalUYU = "UYU " + subtotal;
+    costPlaceholder.innerHTML = subtotalUYU;
+
+    newCost[num] = subtotal;
+
+  }
+  var totalCost = newCost.reduce((a, b) => a + b, 0);
+  currentTotalCost(totalCost);
+}
+
+function currentTotalCost(totalCost)
+{
+  var totalCostPlaceholder = document.getElementById("totalCostPlaceholder");
+  totalCostPlaceholder.innerHTML = "UYU " + totalCost;
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -45,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function(e){
   getJSONData(CART_INFO_URL).then(function(resultObj){
       if (resultObj.status === "ok"){
           showCartList(resultObj.data);
+          newCost = [];
       }
   });
 });
